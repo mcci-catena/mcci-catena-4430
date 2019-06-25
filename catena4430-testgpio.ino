471570308b7e9074e3019a6b6b67eb7876b7c8fd
Terry Moore <tmm@mcci.com>
2019-06-25 14:08:51 -0400

    Now PIR is working

--- catena4430-testgpio.ino
+++ catena4430-testgpio.ino
@@ -2,6 +2,8 @@
 #include <Wire.h>
 #include <Catena.h>
 
+extern McciCatena::Catena gCatena;
+
 /****************************************************************************\
 |
 |   PCA9570 I2C output buffer
@@ -76,7 +78,7 @@ bool cPCA9570::read(std::uint8_t &value) const
     {
     unsigned nRead;
 
-    nRead = this->m_wire->requestFrom(this->m_i2caddr, 1);
+    nRead = this->m_wire->requestFrom(this->m_i2caddr, uint8_t(1));
 
     if (nRead == 0)
         return false;
@@ -89,7 +91,7 @@ bool cPCA9570::read(std::uint8_t &value) const
 
 /****************************************************************************\
 |
-|   The 4430 LED indicator
+|   The 4430 GPIO cluster -- I2C GPIOs plus other LEDs
 |
 \****************************************************************************/
 
@@ -98,11 +100,14 @@ class c4430Gpios
 private:
     static const unsigned kRedLed = D12;
     static const unsigned kDisplayLed = D13;
+
+public:
     static const std::uint8_t kDisplayMask  = (1 << 0);
     static const std::uint8_t kRedMask      = (1 << 1);
-    static const std::uint8_t kGreenMask    = (1 << 2);
-    static const std::uint8_t kBlueMask     = (1 << 3);
+    static const std::uint8_t kBlueMask     = (1 << 2);
+    static const std::uint8_t kGreenMask    = (1 << 3);
 
+private:
     static const std::uint8_t kVout1Mask   = (1 << 0);
     static const std::uint8_t kVout2Mask   = (1 << 1);
 
@@ -133,7 +138,7 @@ bool c4430Gpios::begin()
     {
     bool fResult;
 
-    this->m_gpio->setPolarity(this->m_gpio->getPolarity() | (kBlueMask | kGreenMask));
+    this->m_gpio->setPolarity(this->m_gpio->getPolarity() & ~(kBlueMask | kGreenMask));
 
     fResult = this->m_gpio->begin();
 
@@ -180,6 +185,179 @@ std::uint8_t c4430Gpios::getLeds()
 
 /****************************************************************************\
 |
+|   A simple PIR library -- this uses cPollableObject because it's easier
+|
+\****************************************************************************/
+
+class cPIRdigital : public McciCatena::cPollableObject
+    {
+    static const unsigned getTimeConstant() { return 1000; }
+    static const unsigned kPirData = A0;
+
+public:
+    // constructor
+    cPIRdigital(int pin = kPirData) 
+        : m_pin(pin)
+        { }
+
+    // neither copyable nor movable 
+    cPIRdigital(const cPIRdigital&) = delete;
+    cPIRdigital& operator=(const cPIRdigital&) = delete;
+    cPIRdigital(const cPIRdigital&&) = delete;
+    cPIRdigital& operator=(const cPIRdigital&&) = delete;
+
+    // initialze
+    bool begin();
+
+    // stop operation
+    void end();
+
+    // poll function (updates data)
+    virtual void poll() override;
+
+    // get a reading
+    float read();
+
+private:
+    unsigned m_pin;
+    std::uint32_t m_tLast;
+    float m_value;
+    };
+
+bool cPIRdigital::begin()
+    {
+    pinMode(this->m_pin, INPUT);
+    this->m_value = 0.0;
+
+    // set up for polling.
+    gCatena.registerObject(this);
+    }
+
+void cPIRdigital::poll() /* override */
+    {
+    auto const v = digitalRead(this->m_pin);
+    std::uint32_t tNow = millis();
+
+    float delta = (v ? 1.0f : -1.0f);
+    
+    float m = float(tNow - this->m_tLast) / this->getTimeConstant(); 
+
+    this->m_value = this->m_value + m * (delta  - this->m_value);
+
+    if (this->m_value > 1.0f) 
+        this->m_value = 1.0f;
+    else if (this->m_value < -1.0f)
+        this->m_value = -1.0f;
+
+    this->m_tLast = tNow;
+    }
+
+float cPIRdigital::read()
+    {
+    return this->m_value;
+    }
+
+/****************************************************************************\
+|
+|   A simple timer -- this uses cPollableObject because it's easier
+|
+\****************************************************************************/
+
+class cTimer : public McciCatena::cPollableObject
+    {
+public:
+    // constructor
+    cTimer() {}
+
+    // neither copyable nor movable 
+    cTimer(const cTimer&) = delete;
+    cTimer& operator=(const cTimer&) = delete;
+    cTimer(const cTimer&&) = delete;
+    cTimer& operator=(const cTimer&&) = delete;
+
+    // initialze to fire every nMillis
+    bool begin(std::uint32_t nMillis);
+
+    // stop operation
+    void end();
+
+    // poll function (updates data)
+    virtual void poll() override;
+
+    bool isready();
+    std::uint32_t readTicks();
+    std::uint32_t peekTicks() const;
+
+    void debugDisplay() const
+        {
+        Serial.print("time="); Serial.print(this->m_time);
+        Serial.print(" interval="); Serial.print(this->m_interval);
+        Serial.print(" events="); Serial.print(this->m_events);
+        Serial.print(" overrun="); Serial.println(this->m_overrun); 
+        }
+
+private:
+    std::uint32_t   m_time;
+    std::uint32_t   m_interval;
+    std::uint32_t   m_events;
+    std::uint32_t   m_overrun;
+    };
+
+bool cTimer::begin(std::uint32_t nMillis)
+    {
+    this->m_interval = nMillis;
+    this->m_time = millis();
+    this->m_events = 0;
+
+    // set up for polling.
+    gCatena.registerObject(this);
+
+    return true;
+    }
+
+void cTimer::poll() /* override */
+    {
+    auto const tNow = millis();
+
+    if (tNow - this->m_time >= this->m_interval)
+        {
+        this->m_time += this->m_interval;
+        ++this->m_events;
+
+        /* if this->m_time is now in the future, we're done */
+        if (std::int32_t(tNow - this->m_time) < std::int32_t(this->m_interval))
+            return;
+
+        // rarely, we need to do arithmetic. time and events are in sync.
+        // arrange for m_time to be greater than tNow, and adjust m_events
+        // accordingly. 
+        std::uint32_t const tDiff = tNow - this->m_time;
+        std::uint32_t const nTicks = tDiff / this->m_interval;
+        this->m_events += nTicks;
+        this->m_time += nTicks * this->m_interval;
+        this->m_overrun += nTicks;
+        }
+    }
+
+bool cTimer::isready()
+    {
+    return this->readTicks() != 0;
+    }
+
+std::uint32_t cTimer::readTicks()
+    {
+    auto const result = this->m_events;
+    this->m_events = 0;
+    return result; 
+    }
+
+std::uint32_t cTimer::peekTicks() const
+    {
+    return this->m_events;
+    }
+
+/****************************************************************************\
+|
 |   Variables.
 |
 \****************************************************************************/
@@ -189,6 +367,12 @@ using namespace McciCatena;
 cPCA9570 i2cgpio    { &Wire };
 c4430Gpios gpio     { &i2cgpio };
 Catena gCatena;
+
+cTimer pirPrintTimer;
+cTimer ledTimer;
+cPIRdigital pir;
+
+unsigned ledCount;
 
 /****************************************************************************\
 |
@@ -207,16 +391,47 @@ void setup() {
 
     if (! gpio.begin())
         Serial.println("GPIO failed to initialize");
+
+    pirPrintTimer.begin(2000);
+    ledTimer.begin(200);
+
+    if (! pir.begin())
+        Serial.println("PIR failed to initialize");
+
+    ledCount = 0;
 }
 
 void loop() {
     gCatena.poll();
 
-    for (unsigned count = 4; count > 0; --count)
-        {
-        unsigned iGpio = 4 - count;
+    gpio.setBlue(digitalRead(A0));
 
-        gpio.setLeds(0xFF, 1 << iGpio);
-        delay(100);
+    if (ledTimer.isready())
+        {
+        const std::uint8_t ledMask = (gpio.kDisplayMask | gpio.kRedMask | gpio.kGreenMask);
+        std::uint8_t ledValue;
+
+        unsigned iGpio = 3 - ledCount;
+
+        ledCount = (ledCount + 1) % 3;
+
+        if (ledCount == 0)
+            ledValue = gpio.kDisplayMask;
+        else if (ledCount == 1)
+            ledValue = gpio.kRedMask;
+        else
+            ledValue = gpio.kGreenMask;
+    
+        gpio.setLeds(ledMask, ledValue);
+        }
+
+    if (pirPrintTimer.isready())
+        {
+        float v = (pir.read() + 1.0f) / 2.0f;
+
+        Serial.print(millis());
+        Serial.print(" PIR: ");
+        Serial.print(v * 100.0f);
+        Serial.println("%");
         }
 }
